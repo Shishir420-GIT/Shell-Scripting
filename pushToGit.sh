5d4d764cfdefb97d87297e2d81485da1b311de5f
Shishir420-GIT <66838029+Shishir420-GIT@users.noreply.github.com>
2024-10-10 16:53:30 +0530

    Updating push to git file

--- pushToGit.sh
+++ pushToGit.sh
@@ -1,5 +1,5 @@
-git add .
-git commit -m "Adding All code files."
+git add $1
+git commit -m "$2"
 git branch -M main
 git remote add origin https://github.com/Shishir420-GIT/Shell-Scripting.git
 git push -u origin main
